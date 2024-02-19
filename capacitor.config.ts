cc5c27d9bdabca2f6fe4720eb410c15f5d963e87
lemgabet <59505603+lemgabet12@users.noreply.github.com>
2024-02-19 11:14:18 +0100

    comme

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.templates',
+  appName: 'ionic_ui_templates',
+  webDir: 'www',
+  server: {
+    androidScheme: 'http'
+  }
+};
+
+export default config;
